03c62b5c691e30c00102c777781479445b614bb8
Divya Pandey <divyapandey090901@gmail.com>
2026-07-18 00:33:21 +0530

    feat: implement SwapScreen and add bottom tab navigation

--- Solscan/App.tsx
+++ Solscan/App.tsx
@@ -1,4 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import WalletScreen from "./src/screens/WalletScreen";
+import SwapScreen from "./src/screens/SwapScreen";
+const Tab = createBottomTabNavigator();
 export default function App() {
-  return <WalletScreen />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Wallet" component={WalletScreen} />
+        <Tab.Screen name="Swap" component={SwapScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
